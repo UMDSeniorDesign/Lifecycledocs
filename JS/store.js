d6ae7d3f3519fd8d57dc4d3ceace403c78b2e1a2
ushaniar <aushani@umassd.edu>
2014-02-20 11:49:24 -0500

    new stufft
    take a look

---
+++ JS/store.js
@@ -0,0 +1,9 @@
+function store(i) {
+	return i;
+				/*var XML = loadXML("SoftwareRequirements");
+				var y = XML.documentElement.childNodes;
+				
+				document.cookie = "name=" + y[i].nodeName;
+				document.cookie = "value=" + y[i].childNodes[0].nodeValue;
+				alert(document.cookie);*/
+}
